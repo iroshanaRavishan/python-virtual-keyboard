71bc81a706bc6cdd3db5a1fdcebb5cb6cf8adda3
IroshanaRavishan <bandarairoshana@gamil.com>
2023-11-11 00:14:53 +0530

    added click preset (not added the click event)

--- main.py
+++ main.py
@@ -14,5 +14,25 @@ while True:
     hands, img = detector.findHands(img, flipType=False)
 
     img, bbox = cvzone.putTextRect(img, "How Are You...?", [100, 100], 2, 2, offset=50, border=5)
+
+    if hands:
+        # Get landmarks of the first hand
+        lmList = hands[0]["lmList"]
+        cursor = lmList[8]
+
+        # Get the coordinates of the index finger tip
+        x1, y1 = lmList[8][0], lmList[8][1] # Index 8 corresponds to the index fingertip
+        x2, y2 = lmList[4][0], lmList[4][1] # Index 4 corresponds to the thumb fingertip
+
+        # Display the connecting lines on the image
+        length, info, img = detector.findDistance((x1, y1), (x2, y2), img, (255, 255, 255), 5)
+
+        # Check if the fingers are close enough to complete a CLICK action
+        if length < 50:
+            print("Clicked")
+        else:
+            print("Not")
+
+
     cv2.imshow("Img", img)
     cv2.waitKey(1)
